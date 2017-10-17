66ac1be768adf20547d4ac9af31a744da8c125f5
ivana@yahoo-inc.com <ivana@yahoo-inc.com>
2017-10-17 19:43:17 +0200

    Including the penultimate exercise

--- testing.js
+++ testing.js
@@ -1,14 +1,7 @@
-var t = new Date(Date.now());
-var month = 1 + t.getMonth();
-var formattedDate = t.getFullYear() + "-" + month + "-" + t.getDate() + " " + t.getHours() + ":" + t.getMinutes();
-//t = t.now();
-
-console.log(t);
-console.log(formattedDate);
-// console.log(t.toDateString());
-//
-//
-// var d = new Date(1993, 6, 28, 14, 39, 7);
-//
-// console.log(d.toString());     // logs Wed Jul 28 1993 14:39:07 GMT-0600 (PDT)
-// console.log(d.toDateString()); // logs Wed Jul 28 1993
+for (var i = 1; i <= 25; i++){
+	var digit = i;
+	if (digit < 10){
+		digit = "0"+digit;
+	}
+	console.log("yqa_testuser" + digit);
+}
